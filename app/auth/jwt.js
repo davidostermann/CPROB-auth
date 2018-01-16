76b8a6942e480ea9c8e517b89a3ecc6ebdfa55b3
david <ostermann.david@gmail.com>
2018-01-16 18:02:21 +0100

    verify token

--- app/auth/jwt.js
+++ app/auth/jwt.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const user = 
 
 const extractBearerToken = headerValue => {
   if (typeof headerValue !== "string") {
@@ -11,10 +12,26 @@ const extractBearerToken = headerValue => {
 exports.checkTokenMiddleware = (req, res, next) => {
   // 1. extraction
   const token = req.headers.authorization && extractBearerToken(req.headers.authorization);
+
   // 2. verifier que l'extraction a bien generé un token //if(!token) 
+  if(!token) {
+    return res.status(403).json({error:'Bad token'})
+  }
+
   // 3.verifier la qualité du token via jwt.verify
-  // 4. get id from token payload
-  // 5. get user from id
-  // 6. enrichit le req ave le user recupéré
-  // 7. call next()
+  jwt.verify(token, "coucou", (err, token) => {
+    if(err) {
+      return res.status(403).json({ error: "2. Bad token" });
+    }
+
+    // 4. get id from token payload
+
+    // 5. get user from id
+    // 6. enrichit le req ave le user recupéré
+    // 7. call next()
+
+
+    next()
+  });
+
 }
